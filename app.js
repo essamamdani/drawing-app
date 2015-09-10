3901a518a1da320f247bf7accee59ca6c08c219e
essamamdani <mamdani.info@gmail.com>
2015-09-10 22:12:28 +0500

    fixed download issue

--- app.js
+++ app.js
@@ -6,36 +6,28 @@ var color = $(".selected").css("backgroundColor"),
     context = $canvas[0].getContext("2d"),
     lastevent,
     mousedown = false;
-
-
 function changeColor() {
     var newColor = $(".newColors").map(function () {
         return $(this).val();
     }).get().join(", ");
     $("#newColor").css("background-color", "rgb(" + newColor + ")");
 }
-
 $(".controls").on("click", "li", function () {
     $(this).siblings().removeClass("selected");
     $(this).addClass("selected");
     color = $(this).css("backgroundColor");
 });
-
 $("#revealColorSelect").click(function () {
     changeColor();
     $("#colorSelect").toggle();
 });
-
 $(".newColors").change(changeColor);
-
 $("#addNewColor").click(function () {
     var $newColorRow = $("<li></li>");
     $(".controls ul").append($newColorRow);
     $newColorRow.css("background-color", $("#newColor").css("background-color"));
     $newColorRow.click();
 });
-
-
 $canvas.mousedown(function (e) {
     lastevent = e;
     mousedown = true;
@@ -56,7 +48,10 @@ $canvas.mousedown(function (e) {
 });
 
 $("#downloadbtn").on("click", function () {
-    var dt = $canvas[0].toDataURL("image/png");
-    window.location.href = "data:application/octet-stream;" + dt.split(";")[1];
+    var d = new Date();
+    var timestamp = "IMG " + d.getFullYear() + "-" + d.getMonth() + "-" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+    var dt = $canvas[0].toDataURL("image/png");//.replace("image/png", "image/octet-stream");
+    $(this).attr("href",dt).attr("download",timestamp+".png");
 
 });
+
